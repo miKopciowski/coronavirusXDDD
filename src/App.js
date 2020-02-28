4bd4030d1a77587ef44a154a912c0a0b64faee71
mk <mikolaj-kopciowski@wp.pl>
2020-02-28 17:28:31 +0100

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,138 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import Select from 'react-select';
 import './App.css';
 
-function App() {
+
+const Q1 = [
+  { value: 1, label: 'Chocolate' },
+  { value: 2, label: 'Strawberry' },
+  { value: 3, label: 'Vanilla' },
+  { value: 4, label: 'Vanilla' },
+];
+
+const Q2 = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      miasto: "",
+      goraczka: false,
+      wynik: 0,
+
+    };
+  }
+
+  handleChange1 = miasto => {
+    this.setState({ miasto });
+    console.log(`Option selected:`, this.state.miasto);
+  };
+
+
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <div>
+          <h1>Test na koronawirusa</h1>
+          <p>
+            tutaj jakies pierdolenie ze nie warto sie tym przejmowac
+          </p>
+        </div>
+        <div className="testCard">
+        <div class="shadow p-3 mb-5 bg-white rounded">
+          <div>
+            <h1>Wypełnij test i poznaj swój wynik</h1>
+          </div>
+          <p className="question">W jak duzej miejscowosci mieszkasz</p>
+                <Select
+              value={this.state.miasto}
+              onChange={this.handleChange1}
+              options={Q1}
+            />
+
+          <p className="question">W jakim jestes wieku</p>
+                <Select
+              value={this.state.miasto}
+              onChange={this.handleChange1}
+              options={Q1}
+            />
+
+          <p className="question">Czy miałeś styczność z osobą zarazona</p>
+                <Select
+              value={this.state.miasto}
+              onChange={this.handleChange1}
+              options={Q1}
+            />
+
+          <p className="question">W jak duzej miejscowosci mieszkasz</p>
+                <Select
+              value={this.state.miasto}
+              onChange={this.handleChange1}
+              options={Q1}
+            />
+
+          <p className="question">W jak duzej miejscowosci mieszkasz</p>
+                <Select
+              value={this.state.miasto}
+              onChange={this.handleChange1}
+              options={Q1}
+            />
+
+          <p className="question">W jak duzej miejscowosci mieszkasz</p>
+                <Select
+              value={this.state.miasto}
+              onChange={this.handleChange1}
+              options={Q1}
+            />
+
+          <p className="question">Czy masz aktualnie gorączkę</p>
+                <Select
+              value={this.state.miasto}
+              onChange={this.handleChange1}
+              options={Q1}
+            />
+
+          <p className="question">Czy masz aktualnie kaszel</p>
+                <Select
+              value={this.state.miasto}
+              onChange={this.handleChange1}
+              options={Q1}
+            />
+          <p className="question">Czy masz aktualnie duszności</p>
+          
+        
+         
+         
+          <p className="question">Czy masz aktualnie bóle mięśni</p>
+          
+          <p className="question">Czy masz aktualnie zmęczenie</p>
+
+
+          <p className="question">W jak duzej miejscowosci mieszkasz</p>
+                <Select
+              value={this.state.miasto}
+              onChange={this.handleChange1}
+              options={Q1}
+            />
+
+          <button>Sprawdz swój wynik</button>
+
+          
+        </div>   
+
+    
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
